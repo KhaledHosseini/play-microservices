b0cf4878759645947289a6c8b2c3b3d1d92d6959
Khaled Hosseini <xalid77@gmail.com>
2023-06-19 19:05:45 -0700

    Add build.rs file

---
+++ auth-service/Build.rs
@@ -0,0 +1,13 @@
+use std::{env, path::PathBuf};
+fn main()->Result<(),Box<dyn std::error::Error>>{
+
+   let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+   tonic_build::configure()
+   .build_server(true)
+   //.out_dir(out_dir)
+   .file_descriptor_set_path(out_dir.join("user_descriptor.bin"))
+   .compile(&["./proto/user.proto"],&["proto"])//.compile(&["../proto/user.proto"],&["../proto"])//first argument is .proto file path, second argument is the folder.
+   .unwrap_or_else(|e| panic!("protobuf compile error: {}", e));
+   
+   Ok(())
+}
