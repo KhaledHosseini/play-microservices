e371e580752f008f8d8a754de6b8e561a48b3936
Khaled Hosseini <xalid77@gmail.com>
2023-06-19 19:23:15 -0700

    Initialize Tonik server

--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+mod service;
+mod config;
+use service::{UserService};
+use tonic::{transport::Server};
+use proto::user_server::{UserServer};
+mod proto {
+    include!("proto.rs");
+    pub(crate) const FILE_DESCRIPTOR_SET: &[u8] =
+        tonic::include_file_descriptor_set!("user_descriptor");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let config = Config::init();
+
+    let mut server_addr = "[::0]:".to_string();
+    server_addr.push_str(&config.server_port);
+    let addr: SocketAddr = server_addr.as_str()
+    .parse()
+    .expect("Unable to parse socket address");
+
+    let user_service = UserService::default();
+    let reflection_service = 
+    tonic_reflection::server::Builder::configure()
+.register_encoded_file_descriptor_set(proto::FILE_DESCRIPTOR_SET)
+        .build()
+        .unwrap();
+    Server::builder()
+        .add_service(UserServer::new(user_service))
+        .add_service(reflection_service)
+        .serve(address)
+        .await?;
+    Ok(())
 }
