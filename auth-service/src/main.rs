98d26b2efb82e22730283c1c6a44b1db3b1143e0
Khaled Hosseini <xalid77@gmail.com>
2023-06-19 20:37:43 -0700

    Add missing crates token

--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -1,8 +1,12 @@
-mod service;
+// in the main file, declare all modules of sibling files.
 mod config;
+mod service;
 mod convert;
 mod models;
+mod schema;
+mod token;
 
+//proto
 use service::{UserService};
 use tonic::{transport::Server};
 use proto::user_server::{UserServer};
@@ -12,25 +16,39 @@ mod proto {
         tonic::include_file_descriptor_set!("user_descriptor");
 }
 
+//diesel
 use diesel::{pg::PgConnection, prelude::*};
 use diesel_migrations::EmbeddedMigrations;
 use diesel_migrations::{embed_migrations, MigrationHarness};
 pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("./migrations");
 
-
 use config::Config;
+
 use std::net::SocketAddr;
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let config = Config::init();
+    
+    // env::set_var("RUST_BACKTRACE", "full");
 
+    let config = Config::init();
+    
+    //do migration
     let database_url = config.database_url.clone();
     let mut connection = PgConnection::establish(&database_url)
     .unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
-
     _ = connection.run_pending_migrations(MIGRATIONS);
 
+    //user service
+    let redis_url = config.redis_url.clone();
+    let user_service = UserService::new(&database_url, &redis_url)?;
+
+    //reflection service for user
+    let reflection_service = tonic_reflection::server::Builder::configure()
+        .register_encoded_file_descriptor_set(proto::FILE_DESCRIPTOR_SET)
+        .build()
+        .unwrap();
 
     let mut server_addr = "[::0]:".to_string();
     server_addr.push_str(&config.server_port);
@@ -38,13 +56,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     .parse()
     .expect("Unable to parse socket address");
 
-    let user_service = UserService::default();
-    let reflection_service = 
-    tonic_reflection::server::Builder::configure()
-.register_encoded_file_descriptor_set(proto::FILE_DESCRIPTOR_SET)
-        .build()
-        .unwrap();
-
+    println!("Server listening on {}", addr);
 
     Server::builder()
         .add_service(UserServer::new(user_service))
