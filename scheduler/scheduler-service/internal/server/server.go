42299dbf8ea8e91d2bf2a1b09c60c43a1bcd0b82
Khaled Hosseini <xalid77@gmail.com>
2023-06-30 12:36:54 -0700

    Update server.go to include database, scheduler and job producer

--- scheduler/scheduler-service/internal/server/server.go
+++ scheduler/scheduler-service/internal/server/server.go
@@ -1,24 +1,35 @@
 package server
 
 import (
+	"context"
 	"log"
 	"net"
 
 	"github.com/KhaledHosseini/play-microservices/scheduler/scheduler-service/config"
 	MyJobGRPCService "github.com/KhaledHosseini/play-microservices/scheduler/scheduler-service/internal/models/job/grpc"
+	"github.com/KhaledHosseini/play-microservices/scheduler/scheduler-service/pkg/logger"
 	JobGRPCServiceProto "github.com/KhaledHosseini/play-microservices/scheduler/scheduler-service/proto"
 
+	JobDB "github.com/KhaledHosseini/play-microservices/scheduler/scheduler-service/internal/models/job/database"
+	kafka "github.com/KhaledHosseini/play-microservices/scheduler/scheduler-service/internal/models/job/message_broker"
+
+	"github.com/reugn/go-quartz/quartz"
+	kg "github.com/segmentio/kafka-go"
+	"go.mongodb.org/mongo-driver/mongo"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
 type server struct {
-	cfg *config.Config
+	log       logger.Logger
+	cfg       *config.Config
+	mongoDB   *mongo.Client
+	kafkaConn *kg.Conn
 }
 
 // NewServer constructor
-func NewServer(cfg *config.Config) *server {
-	return &server{cfg: cfg}
+func NewServer(log logger.Logger, cfg *config.Config, mongoDB *mongo.Client, kafkaConn *kg.Conn) *server {
+	return &server{log: log, cfg: cfg, mongoDB: mongoDB, kafkaConn: kafkaConn}
 }
 
 func (s *server) Run() error {
@@ -27,9 +38,25 @@ func (s *server) Run() error {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	grpc_server := grpc.NewServer()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-	job_service := MyJobGRPCService.NewJobService()
+	sched := quartz.NewStdScheduler()
+	sched.Start(ctx)
+	defer func() {
+		sched.Stop()
+		sched.Wait(ctx)
+	}()
+
+	grpc_server := grpc.NewServer()
+	job_db := JobDB.NewJobDBMongo(s.mongoDB)
+
+	job_producer := kafka.NewJobsProducer()
+	job_producer.Run(s.kafkaConn, s.cfg)
+	defer job_producer.Close()
+
+	job_service := MyJobGRPCService.NewJobService(s.log, job_db, job_producer, sched)
+	job_service.LoadScheduledJobs()
 	JobGRPCServiceProto.RegisterJobServiceServer(grpc_server, job_service)
 	reflection.Register(grpc_server)
 
