b5e1cc0023dfbc43d089134430c24acb26856c2f
Khaled Hosseini <xalid77@gmail.com>
2023-06-30 12:38:15 -0700

    Add mongodb initializer package

---
+++ scheduler/scheduler-service/pkg/mongodb/mongodb.go
@@ -0,0 +1,43 @@
+package mongodb
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	connectTimeout  = 30 * time.Second
+	maxConnIdleTime = 3 * time.Minute
+	minPoolSize     = 20
+	maxPoolSize     = 300
+)
+
+// NewMongoDBConn Create new MongoDB client
+func NewMongoDBConn(ctx context.Context, URI string, user string, pass string) (*mongo.Client, error) {
+	client, err := mongo.NewClient(
+		options.Client().ApplyURI(URI).
+			SetAuth(options.Credential{
+				Username: user,
+				Password: pass,
+			}).
+			SetConnectTimeout(connectTimeout).
+			SetMaxConnIdleTime(maxConnIdleTime).
+			SetMinPoolSize(minPoolSize).
+			SetMaxPoolSize(maxPoolSize))
+	if err != nil {
+		return nil, err
+	}
+
+	if err := client.Connect(ctx); err != nil {
+		return nil, err
+	}
+
+	if err := client.Ping(ctx, nil); err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
