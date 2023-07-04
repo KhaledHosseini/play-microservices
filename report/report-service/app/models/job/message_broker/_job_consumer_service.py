0c41dd21e3f83ad607404fd0cc66a27110463433
Khaled Hosseini <xalid77@gmail.com>
2023-07-04 15:18:53 -0700

    Refactor models/job

--- report/report-service/app/models/job/message_broker/_job_consumer_service.py
+++ report/report-service/app/models/job/message_broker/_job_consumer_service.py
@@ -1,11 +1,16 @@
 from threading import Thread
 import logging
+from datetime import datetime
+from typing import List
 
 from config import Config
+
+from app.message_engines import KafkaConsumerWorker
+
 from app.models import ReportDBInterface
-
-from app.models.job.message_broker._job_consumer_worker import JobConsumerWorker
-
+from app.models import ReportDBInterface
+from app.models import Job
+from app.models import Report
 
 class ConsumerService:
   def __init__(self,cfg: Config, reportDb: ReportDBInterface):
@@ -36,3 +41,29 @@ class ConsumerService:
                            kafkaBrokers=self.cfg.KafkaBrokers,
                            reportDB= self.reportDB)
     consumer.run()
+
+class JobConsumerWorker(KafkaConsumerWorker):
+    def __init__(
+        self,
+        topic: str,
+        consumerGroupId: str,
+        kafkaBrokers: List[str],
+        reportDB: ReportDBInterface,
+    ):
+        super().__init__(
+            topic=topic, consumerGroupId=consumerGroupId, kafkaBrokers=kafkaBrokers
+        )
+        self.reportDB = reportDB
+
+    def messageRecieved(self, topic: str, message: any):
+        try:
+            job = Job.from_dict(message)
+            report = Report(
+                type=0,
+                topic=topic,
+                created_time=datetime.now(),
+                report_data=job.toJsonStr(),
+            )
+            self.reportDB.create(report=report)
+        except Exception as e:
+            logging.error("Unable to load json for job ", e)
